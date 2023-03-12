df67f4665d5e81fa502b648c4f39dd7ce1ae56d3
badass-ivan <vanya_kuzin_97@list.ru>
2023-03-12 19:07:59 +0300

    Fix

--- src/utils/bind-logs.ts
+++ src/utils/bind-logs.ts
@@ -1,18 +1,13 @@
 import { BotService } from "../services/bot.service";
-import config from "../config";
 
 const defaultLog = console.log;
 console.log = (...args) => {
-    if (config.NODE_ENV === "production")
-        BotService.sendMsgToAdmin(JSON.stringify(args))
-
+    BotService.sendMsgToAdmin(JSON.stringify(args))
     defaultLog(...args, new Date())
 }
 
 const defaultError = console.error;
 console.error = (...args) => {
-    if (config.NODE_ENV === "production")
-        BotService.sendErrorToAdmin(JSON.stringify(args))
-
+    BotService.sendErrorToAdmin(JSON.stringify(args))
     defaultError(...args, new Date())
 }
